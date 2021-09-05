a514d011973fce5e452ad89dbdd08d9a62cf4f06
Igor Kamyshev <igor@kamyshev.me>
2021-09-05 16:03:58 +0700

    Remove abusive ssl

--- app/utils/getDbClient.js
+++ app/utils/getDbClient.js
@@ -1,22 +1,17 @@
 import PG from "pg";
-import { readFileSync } from "fs";
-import { join } from "path";
 
 export const getDbClient = ({ config }) => {
-  const getSslConfig = config.isDev()
-    ? () => undefined
-    : () => ({
-        ca: readFileSync(
-          join(__dirname, "..", "..", ".secure", "ca-certificate.txt")
-        ),
-      });
-
   return new PG.Client({
     user: config.getOrThrow("DB_USER"),
     database: config.getOrThrow("DB_NAME"),
     password: config.getOrThrow("DB_PASSWORD"),
     port: config.getOrThrow("DB_PORT"),
     host: config.getOrThrow("DB_HOST"),
-    ssl: getSslConfig(),
+    ssl: config.isProd()
+      ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      : undefined,
   });
 };
