28f5ca178664a7ce58db43f65b15a4a709b7bbda
Igor Kamyshev <igor@kamyshev.me>
2021-09-05 19:23:18 +0700

    Try to switch on connection string

--- app/utils/getDbClient.js
+++ app/utils/getDbClient.js
@@ -1,17 +1,25 @@
 import PG from "pg";
 
 export const getDbClient = ({ config }) => {
-  return new PG.Client({
-    user: config.getOrThrow("DB_USER"),
-    database: config.getOrThrow("DB_NAME"),
-    password: config.getOrThrow("DB_PASSWORD"),
-    port: config.getOrThrow("DB_PORT"),
-    host: config.getOrThrow("DB_HOST"),
-    ssl: config.isProd()
-      ? {
-          require: true,
-          rejectUnauthorized: false,
-        }
-      : undefined,
-  });
+  let pgConfig;
+
+  if (config.isDev()) {
+    pgConfig = {
+      user: config.getOrThrow("DB_USER"),
+      database: config.getOrThrow("DB_NAME"),
+      password: config.getOrThrow("DB_PASSWORD"),
+      port: config.getOrThrow("DB_PORT"),
+      host: config.getOrThrow("DB_HOST"),
+    };
+  } else {
+    pgConfig = {
+      connectionString: config.getStringOrThrow("DATABASE_URL"),
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    };
+  }
+
+  return new PG.Client(pgConfig);
 };
